2a444fddf5e54fcb78ae8401f2a9c8a927abed0b
darko95 <djuricdarko95@gmail.com>
2024-04-29 15:11:23 +0200

    #2
    add & update employee(not completed)

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import { listEmployees } from '../services/EmployeeService';
+import { useNavigate } from 'react-router-dom';
 
 const ListEmployeeComponent = () => {
 
     const [employees, setEmployees] = useState([]);
+    const navigator = useNavigate();
 
     useEffect(() => {
         listEmployees().then((response) => {
@@ -13,9 +15,18 @@ const ListEmployeeComponent = () => {
         })
     }, [])
 
+    function addNewEmployee(){
+        navigator('/add-employee')
+    }
+
+    const updateEmployee = (id)=>{
+        navigator(`/edit-employee/${id}`);
+    }
+
   return (
     <div className='container'>
         <h2 className='text-center'>List of Employees</h2>
+        <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add employee</button>
         <table className='table table-striped table-bordered'>
             <thead>
                 <tr>
@@ -23,6 +34,7 @@ const ListEmployeeComponent = () => {
                     <th>Employee First Name</th>
                     <th>Employee Last Name</th>
                     <th>Employee Email</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -33,6 +45,9 @@ const ListEmployeeComponent = () => {
                             <td>{employee.firstName}</td>
                             <td>{employee.lastName}</td>
                             <td>{employee.email}</td>
+                            <td>
+                                <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                            </td>
                         </tr>)
                 }
             </tbody>
